fe35c9e580ddb014982ef5ae861da06a88a058ff
watermell0n <144258154+watermell0n@users.noreply.github.com>
2025-08-21 01:06:13 +0900

    [level 1] Title: 카드 뭉치, Time: 0.34 ms, Memory: 31.2 MB -BaekjoonHub

--- C#/프로그래머스/1/159994. 카드 뭉치/README.md
+++ C#/프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
 
 ### 성능 요약
 
-메모리: 31.1 MB, 시간: 0.21 ms
+메모리: 31.2 MB, 시간: 0.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 21일 01:02:29
+2025년 08월 21일 01:06:11
 
 ### 문제 설명
 
--- C#/프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cs
+++ C#/프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cs
@@ -4,19 +4,18 @@ public class Solution {
     public string solution(string[] cards1, string[] cards2, string[] goal) {
         string answer = "Yes";
         int i = 0; int j = 0;
-        int num = 0;
-        
-        while (num<goal.Length)
+
+        for (int num=0; num<goal.Length; num++)
         {
             if (i<cards1.Length && goal[num]==cards1[i])
             {
-                num++;
                 i++;
+                continue;
             }
             else if (j<cards2.Length && goal[num]==cards2[j])
             {
-                num++;
                 j++;
+                continue;
             }
             else
             {
