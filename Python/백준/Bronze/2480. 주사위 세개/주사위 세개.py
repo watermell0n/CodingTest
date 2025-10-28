a5e67ac33d02d1e61ace6245d53fc1a24b9d7674
watermell0n <144258154+watermell0n@users.noreply.github.com>
2025-10-29 02:29:12 +0900

    [Bronze IV] Title: 주사위 세개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ Python/백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,12 +1,10 @@
 x, y, z = map(int, input().split())
 if (x==y==z):
     print(10000+x*1000)
-elif (x==y!=z):
+elif (x==y or x==z):
     print(1000+x*100)
-elif (x!=y==z):
+elif (y==z):
     print(1000+y*100)
-elif (x==z!=y):
-    print(1000+x*100)
 else:
     n = max(x,y,z)
     print(n*100)
