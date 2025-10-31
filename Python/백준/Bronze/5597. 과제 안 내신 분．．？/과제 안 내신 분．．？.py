51ad3e0a7d8ce18ae002cf6998bfe439513d5fcc
watermell0n <144258154+watermell0n@users.noreply.github.com>
2025-11-01 00:42:59 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ Python/백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,10 +1,7 @@
-arr = [0 for i in range(30)]
+arr = [i+1 for i in range(30)]
 for j in range(28):
     n = int(input())
-    arr[n-1] = 1
-arr2 = list()
-for k in range(30):
-    if (arr[k]==0):
-        arr2.append(k+1)
-print(min(arr2))
-print(max(arr2))
+    arr.remove(n)
+
+print(arr[0])
+print(arr[1])
