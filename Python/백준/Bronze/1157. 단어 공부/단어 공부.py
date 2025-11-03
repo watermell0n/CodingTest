40277dff5cf1bd0ac5859920831a8fc9deeb9d94
watermell0n <144258154+watermell0n@users.noreply.github.com>
2025-11-03 20:46:44 +0900

    [Bronze I] Title: 단어 공부, Time: 184 ms, Memory: 34368 KB -BaekjoonHub

--- Python/백준/Bronze/1157. 단어 공부/단어 공부.py
+++ Python/백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,18 +1,10 @@
-word = input()
-word = [s.upper() for s in word]
+word = input().upper()
 alphabet = [0] * 26
-maxNum = 0
-maxWord = ''
 
 for i in word:
-    num = ord(i)-65
-    alphabet[num] += 1
-    if alphabet[num] > maxNum: 
-        maxNum = alphabet[num]
-        maxWord = i
-
-same = 0
-for j in alphabet:
-    if maxNum == j: same += 1
+    alphabet[ord(i)-65] += 1
     
-print('?' if same >=2 else maxWord)
+maxNum = max(alphabet)
+
+n = alphabet.count(maxNum)    
+print('?' if n>1 else chr(alphabet.index(maxNum)+65))
