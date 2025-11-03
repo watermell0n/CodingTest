f70ea38cd473bbf68255d997b512fe74b5dcaaed
watermell0n <144258154+watermell0n@users.noreply.github.com>
2025-11-03 21:00:19 +0900

    [Bronze I] Title: 단어 공부, Time: 68 ms, Memory: 34368 KB -BaekjoonHub

--- Python/백준/Bronze/1157. 단어 공부/단어 공부.py
+++ Python/백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,4 +1,4 @@
-word = input().upper()
+'''word = input().upper()
 alphabet = [0] * 26
 
 for i in word:
@@ -7,4 +7,17 @@ for i in word:
 maxNum = max(alphabet)
 
 n = alphabet.count(maxNum)    
-print('?' if n>1 else chr(alphabet.index(maxNum)+65))
+print('?' if n>1 else chr(alphabet.index(maxNum)+65))'''
+
+
+'''중복 제거하는 집합 자료형 사용'''
+word = input().upper()
+sWord = list(set(word))
+num = []
+for ch in sWord:
+    num.append(word.count(ch))
+cnt = max(num)
+if (num.count(cnt) > 1 ):
+    print('?')
+else:
+    print(sWord[num.index(cnt)])
