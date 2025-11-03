41947cdca9d31acc1d59c1125034e1288506cbc5
watermell0n <144258154+watermell0n@users.noreply.github.com>
2025-11-03 19:36:27 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ Python/백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,10 +1,5 @@
 import sys
 input = sys.stdin.readline
 
-answer = True
 word = input().strip()
-for i in range(len(word)//2):
-    if (word[i] != word[-(i+1)]):
-        answer = False
-        break
-print(1) if (answer==True) else print(0)
+print(1) if (word == word[::-1]) else print(0)
