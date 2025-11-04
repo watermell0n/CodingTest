f844f783555329f92512f080ac1c07c17bc43b62
watermell0n <144258154+watermell0n@users.noreply.github.com>
2025-11-05 04:29:08 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ Python/백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -3,14 +3,8 @@ input = sys.stdin.readline
 
 croatia = ['c=','c-','dz=','d-','lj','nj','s=','z=']
 word = input().strip()
-num = 0
+
 for ch in croatia:
-    if (ch in word):
-        while (word.count(ch) > 0):
-            num += 1
-            idx = word.find(ch)
-            word = word[:idx] + ' ' + word[idx+len(ch):]
-word = word.replace(' ','')
-for i in word:
-    num += 1
-print(num)
+    word = word.replace(ch, '*')
+    
+print(len(word))
