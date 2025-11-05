3581b775e03df03bbdb4304f7d0029d3595ec810
watermell0n <144258154+watermell0n@users.noreply.github.com>
2025-11-05 15:18:41 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ Python/백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,21 +1,28 @@
 import sys
 input = sys.stdin.readline
+
 num = int(input())
+count = num
+
 for i in range(num):
-    word = input()
-    exist = []
-    while (len(word) > 0):
-        if word=='\n': break
-        ch = word[0]
-        exist.append(ch)
-        j = 0
-        while (word[j] == ch):
-            j += 1
-        word = word[j:]
-        
-    # 중복 문자 검사  
-    for ex in exist:
-        if (exist.count(ex) > 1):
-            num -= 1
+    word = input().strip()
+    for c in range(len(word)-1):
+        if (word[c] == word[c+1]):
+            pass
+        elif (word[c] in word[c+1:]):
+            count -= 1
             break
-print(num)
+print(count)
+
+'''
+    exist = set()
+    prev = ''
+    
+    for ch in word:
+        if (ch != prev):
+            if (ch in exist):
+                count -= 1
+                break
+            exist.add(ch)
+        prev = ch        
+print(count)'''
