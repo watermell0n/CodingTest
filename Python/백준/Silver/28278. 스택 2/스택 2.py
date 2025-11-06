0173bfed5c3168551ce4b2ffeb271927f7a35aba
watermell0n <144258154+watermell0n@users.noreply.github.com>
2025-11-07 02:40:34 +0900

    [Silver IV] Title: 스택 2, Time: 1220 ms, Memory: 72404 KB -BaekjoonHub

---
+++ Python/백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+stack = []
+for i in range(n):
+    order = list(map(int, input().strip().split()))
+    if order[0]==1:
+        stack.append(order[1])
+    elif order[0]==2:
+        if (len(stack)>0): print(stack.pop())
+        else: print(-1)
+    elif order[0]==3:
+        print(len(stack))
+    elif order[0]==4:
+        if (len(stack)==0): print(1)
+        else: print(0)
+    else:
+        if (len(stack)>0): print(stack[-1])
+        else: print(-1)
