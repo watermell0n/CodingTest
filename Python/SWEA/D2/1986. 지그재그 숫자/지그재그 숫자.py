9ca0568b4edbc7d3be3ff31a1087525404402b53
watermell0n <144258154+watermell0n@users.noreply.github.com>
2025-11-14 18:10:21 +0900

    [D2] Title: 지그재그 숫자, Time: 57 ms, Memory: 50,816 KB -BaekjoonHub

--- Python/SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.py
+++ Python/SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.py
@@ -40,11 +40,7 @@ T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     n = int(input())
-    odd_s = 0
-    even_s = 0
+    sum = 0
     for i in range(1, n+1):
-        if (i%2)==1:
-            odd_s += i
-        else:
-            even_s += i
-    print(f"#{test_case} {odd_s-even_s}")
+        sum += i if i%2==1 else -i
+    print(f"#{test_case} {sum}")
