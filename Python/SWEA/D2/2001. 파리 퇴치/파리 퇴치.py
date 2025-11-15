b386bda2af83c9fc48ebc51f72c02406943cf3f1
watermell0n <144258154+watermell0n@users.noreply.github.com>
2025-11-15 18:04:19 +0900

    [D2] Title: 파리 퇴치, Time: 60 ms, Memory: 53,632 KB -BaekjoonHub

--- Python/SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ Python/SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -3,12 +3,15 @@ T = int(input())
 for test_case in range(1, T + 1):
     n, m = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(n)]
+    prefixSum = [[0]*(n+1) for x in range(n+1)]
     s_max = 0
+    for i in range(1, n+1):
+        for j in range(1,n+1):
+            prefixSum[i][j] = prefixSum[i-1][j] + prefixSum[i][j-1] - prefixSum[i-1][j-1] + arr[i-1][j-1]
     
-    for i in range(n-m+1):
-        for j in range(n-m+1):
-            c_arr = [row[i:i+m] for row in arr[j:j+m]]
-            s = sum(sum(row) for row in c_arr)
-            if s > s_max:
-                s_max = s
+    for x in range(m,n+1):
+        for y in range(m,n+1):
+            s = prefixSum[x][y] - prefixSum[x-m][y] - prefixSum[x][y-m] + prefixSum[x-m][y-m]
+            s_max = max(s_max, s)
+            
     print(f"#{test_case} {s_max}")
