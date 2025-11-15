23a4bf8c06844f90fb41272d2ac4711bc09b86eb
watermell0n <144258154+watermell0n@users.noreply.github.com>
2025-11-15 18:55:03 +0900

    [D2] Title: 스도쿠 검증, Time: 60 ms, Memory: 51,712 KB -BaekjoonHub

--- Python/SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ Python/SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -4,23 +4,25 @@ for test_case in range(1, T + 1):
     arr = [list(map(int, input().split())) for _ in range(9)]
     flag = 1
     for i in range(9):
-        if (sum(arr[i]) != 45):
-            flag = 0
-            break
-        s = 0
+        s_row = set()
+        s_col = set()
         for j in range(9):
-            s += arr[j][i]
-        if (s != 45):
+            s_row.add(arr[i][j])	# 행 검사
+            s_col.add(arr[j][i])	 # 열 검사
+        if (len(s_row)!=9 or len(s_col)!=9):
             flag = 0
             break
-            
+    # 3x3 검사
     if (flag==1):
         for x in range(0,9,3):
-            if (flag==1):
-                for y in range(0,9,3):
-                    c_arr = [row[y:y+3] for row in arr[x:x+3]]
-                    s = sum(sum(row) for row in c_arr)
-                    if (s != 45):
-                        flag = 0
-                        break
+            for y in range(0,9,3):
+                block = set()
+                for p in range(3):
+                    for q in range(3):
+                        block.add(arr[x+p][y+q])
+                if (len(block) != 9):
+                    flag = 0
+                    break
+            if (flag==0):
+                break
     print(f"#{test_case} {flag}")
