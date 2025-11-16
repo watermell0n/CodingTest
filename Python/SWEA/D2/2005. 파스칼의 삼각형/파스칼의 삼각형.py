0c71bd9e364a37c6160a16d2170c0fc783724198
watermell0n <144258154+watermell0n@users.noreply.github.com>
2025-11-16 18:18:18 +0900

    [D2] Title: 파스칼의 삼각형, Time: 60 ms, Memory: 51,328 KB -BaekjoonHub

--- Python/SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
+++ Python/SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -3,17 +3,13 @@ T = int(input())
 for test_case in range(1, T + 1):
     n = int(input())
     arr = [[0]*(c+1) for c in range(n)]
-    arr[0][0] = 1
-    if (n>=2):
-        arr[1][0] = 1
-        arr[1][1] = 1
-    if (n>=3):
-        for i in range(2,n):
-            for j in range(i+1):
-                if (j==0 or j==i):
-                    arr[i][j] = 1
-                else:
-                    arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
+    
+    for i in range(n):
+        arr[i][0] = 1
+        arr[i][-1] = 1
+        for j in range(1,i):
+            arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
+
     print(f"#{test_case}")
     for r in arr:
         print(*r)
