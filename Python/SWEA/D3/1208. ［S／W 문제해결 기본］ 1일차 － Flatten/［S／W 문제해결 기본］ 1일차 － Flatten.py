29467d4c11b549fa35db983bba640ee244c6ae24
watermell0n <144258154+watermell0n@users.noreply.github.com>
2025-11-19 20:49:59 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 64 ms, Memory: 54,016 KB -BaekjoonHub

--- Python/SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ Python/SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -1,18 +1,8 @@
 for test_case in range(1, 11):
     dump = int(input())
     arr = list(map(int, input().split()))
-    isDone = False
     for i in range(dump):
-        maxH = max(arr)
-        minH = min(arr)
-        if (maxH-minH <=1):
-            isDone = True
-            break
-        else:
-            maxI = arr.index(maxH)
-            minI = arr.index(minH)
-            arr[maxI] -= 1
-            arr[minI] += 1
-    if (not isDone):
-        h = max(arr)-min(arr)
-    print(f"#{test_case} {h}")
+        arr.sort()
+        arr[0] += 1
+        arr[-1] -= 1
+    print(f"#{test_case} {max(arr)-min(arr)}")
