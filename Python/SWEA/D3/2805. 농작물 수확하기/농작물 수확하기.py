7899148b961bf58b8122398cc6139803b25c1b75
watermell0n <144258154+watermell0n@users.noreply.github.com>
2025-11-20 15:40:57 +0900

    [D3] Title: 농작물 수확하기, Time: 109 ms, Memory: 60,800 KB -BaekjoonHub

--- Python/SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
+++ Python/SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -1,12 +1,11 @@
 T = int(input())
 for test_case in range(1, T + 1):
     n = int(input())
-    arr = [list(input()) for _ in range(n)]
+    arr = [list(map(int, input())) for _ in range(n)]
     ans = 0
     m = n//2
-    ans += sum(int(arr[m][k]) for k in range(n))
-    for i in range(1, m+1):
-        for j in range(i, n-i):
-            ans += int(arr[m-i][j])
-            ans += int(arr[m+i][j])
+    for i in range(n):
+        dis = abs(m-i)	# 중간 행과의 거리
+        for j in range(dis, n-dis):
+            ans += arr[i][j]
     print(f"#{test_case} {ans}")
