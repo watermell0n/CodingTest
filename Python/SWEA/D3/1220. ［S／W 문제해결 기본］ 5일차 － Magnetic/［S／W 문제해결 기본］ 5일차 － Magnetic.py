eb1e924ad0c319134cec2e6394909022b7dcf08f
watermell0n <144258154+watermell0n@users.noreply.github.com>
2025-11-20 16:40:40 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 147 ms, Memory: 60,544 KB -BaekjoonHub

--- Python/SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
+++ Python/SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
@@ -3,6 +3,7 @@ for test_case in range(1, T + 1):
     n = int(input())
     rect = [list(map(int, input().split())) for _ in range(n)]
     cnt = 0
+    ''' 1과 2만 묶일 수 있는 것 찾아 cnt 세기
     for j in range(n):
         prev = 0
         for i in range(n):
@@ -11,5 +12,11 @@ for test_case in range(1, T + 1):
             elif (rect[i][j]==2 and prev==1):
                 cnt += 1
                 prev = 0
-            
+    '''
+    
+    # 1과 2로만 이루어진 문자열로 만들어 '12' 개수 세기
+    for i in range(n):
+        cp = [row[i] for row in rect]
+        st = ''.join(map(str, cp)).replace('0','')
+        cnt += st.count('12')
     print(f"#{test_case} {cnt}")
